aa3fe3c17f8770ec499ef8347bb01072f2d85334
Rahmat Wahyu Hadi <hrahmatwahyu@gmail.com>
2020-03-05 17:33:59 +0700

    Add : to database sqlite

--- git/github-serv.go
+++ git/github-serv.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"os"
+	"pandawa/database"
 
 	"golang.org/x/oauth2"
 )
@@ -30,7 +31,7 @@ func InitialSearch(user string) {
 
 // GetUserFromOrg to get list user from org
 // and save from db ->
-func GetUserFromOrg(org string) {
+func GetUserFromOrg(org, ops string) {
 	gitoken, err := GetInitialToken()
 	if err != nil {
 		fmt.Println(err)
@@ -41,10 +42,17 @@ func GetUserFromOrg(org string) {
 	ts := oauth2.StaticTokenSource(
 		&oauth2.Token{AccessToken: gitoken},
 	)
-
 	tc := oauth2.NewClient(ctx, ts)
-	members, err := GetListMemberFromOrg(tc, org)
 
+	// generate tables if ops not null
+	// with name table : github_org
+	if ops != "" {
+		query := "CREATE TABLE IF NOT EXISTS github_org (id INTEGER PRIMARY KEY, user TEXT, org TEXT, url TEXT)"
+		operation := "pandawa-output/" + ops + ".db"
+		database.GenerateTables(operation, query)
+	}
+
+	members, err := GetListMemberFromOrg(tc, org)
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -53,7 +61,12 @@ func GetUserFromOrg(org string) {
 	for _, member := range members {
 		fmt.Println("[+] User : ", *member.Login)
 		fmt.Println("[+] Github : ", *member.HTMLURL)
+		operation := "pandawa-output/" + ops + ".db"
+		queryInsert := "INSERT INTO github_org (" + *member.Login + "," + *member.HTMLURL + "," + org + ") VALUES (?, ?, ?)"
+		database.CreateData(operation, queryInsert)
 	}
+
+	return
 	// fmt.Println(members)
 
 }
